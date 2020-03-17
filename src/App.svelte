a5af7ea0e74791f0063ecdc423d6bbd44bbc7df5
Denis <33554650+denisnow@users.noreply.github.com>
2020-03-17 14:30:36 +0100

    implemented 'openAboutBtn' support

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,37 @@
 <script>
 	import Board from './Board.svelte';
+
+	let isAboutVisible = false;
+	const toggleAboutVisibility = () => isAboutVisible = !isAboutVisible;
 </script>
 
 <h1 class="visuallyHidden">The 15-puzzle game</h1>
 
-<section class="board">
-    <h2 class="visuallyHidden">The game board</h2>
-    <button class="shuffleBtn hidden" title="Shuffle the tiles">
-		<span class="visuallyHidden">Shuffle</span>
-	</button>
-    <button class="openAboutBtn" title="Display information about the game">
-		<span class="visuallyHidden">Information</span>
-	</button>
+{#if !isAboutVisible}
+	<section class="board">
+		<h2 class="visuallyHidden">The game board</h2>
+		<button class="shuffleBtn hidden" title="Shuffle the tiles">
+			<span class="visuallyHidden">Shuffle</span>
+		</button>
+		<button class="openAboutBtn" title="Display information about the game" on:click={toggleAboutVisibility}>
+			<span class="visuallyHidden">Information</span>
+		</button>
 
-    <Board/>
-</section>
-
-<section class="textArea about hidden">
-	<h2 class="visuallyHidden">Information about the game</h2>
-	<button class="closeAboutBtn" title="Return to the game board">
-		<span class="visuallyHidden">Close</span>
-	</button>
-	<div class="textWrapper">
-		<div class="textContainer">
-			<h3>Rules</h3>
-			<p>Place the tiles in ascending order.</p>
-			<h3>Hints</h3>
-			<p>Sort the first two rows, then place the «9» and the «13» tile on their destination places, finally sort remaining tiles.</p>
+		<Board/>
+	</section>
+{:else}
+	<section class="textArea about">
+		<h2 class="visuallyHidden">Information about the game</h2>
+		<button class="closeAboutBtn" title="Return to the game board" on:click={toggleAboutVisibility}>
+			<span class="visuallyHidden">Close</span>
+		</button>
+		<div class="textWrapper">
+			<div class="textContainer">
+				<h3>Rules</h3>
+				<p>Place the tiles in ascending order.</p>
+				<h3>Hints</h3>
+				<p>Sort the first two rows, then place the «9» and the «13» tile on their destination places, finally sort remaining tiles.</p>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+{/if}
