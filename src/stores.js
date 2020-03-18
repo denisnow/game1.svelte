2f7f0cc2ee66a4761be66e2bf23292a7dac7b564
Denis <33554650+denisnow@users.noreply.github.com>
2020-03-18 13:08:14 +0100

    implemented checking if tiles are sorted

--- src/stores.js
+++ src/stores.js
@@ -13,8 +13,26 @@ export const positions = derived( matrix, $matrix => {
     return positions;
 } );
 
+// ==================== MOVING TILES ====================
+
 export const clickedTilePosition = writable(undefined);
 
 clickedTilePosition.subscribe( position => {
     if (position) matrix.update( matrix => moveTiles(matrix, position) );
+} );
+
+// ================= CHECKING IS SORTED =================
+
+export const isSorted = writable(false);
+
+positions.subscribe( positions => {
+    if (positions[0].n === 3 && positions[0].m === 3) {
+        for (let i = 15; i > 0; i--) {
+            let m = Math.floor((i-1)/4),
+                n = i-1-(m*4);
+
+            if (positions[i].m !== m || positions[i].n !== n) return;
+            if (i === 1) isSorted.set(true);
+        }
+    }
 } );
