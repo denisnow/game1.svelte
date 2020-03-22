f1c80b0afa9aacdc141a30e4cd7444d5a8241923
Denis <33554650+denisnow@users.noreply.github.com>
2020-03-22 12:42:58 +0100

    renamed 'tile' variable to 'thisElement'

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -5,18 +5,18 @@
     export let number;
 
     let position,
-        tile;
+        thisElement;
 
     const tileClickHandler = () => {
-        if (tile.classList.contains('clickable')) $clickedTilePosition = position;
+        if (thisElement.classList.contains('clickable')) $clickedTilePosition = position;
     };
 
     const doClickable = () => {
-        if (!tile.classList.contains('clickable')) tile.classList.add('clickable');
+        if (!thisElement.classList.contains('clickable')) thisElement.classList.add('clickable');
     };
 
     const doUnclickable = () => {
-        if (tile.classList.contains('clickable')) tile.classList.remove('clickable');
+        if (thisElement.classList.contains('clickable')) thisElement.classList.remove('clickable');
     };
 
     $: isClickable = $positions[number].n === $positions[0].n || $positions[number].m === $positions[0].m;
@@ -24,7 +24,7 @@
     afterUpdate( () => {
         if (!position || position.n !== $positions[number].n || position.m !== $positions[number].m) {
             position = $positions[number];
-            tile.style.transform = 'translate(' + position.n*100 + '%, ' + position.m*100 + '%)';
+            thisElement.style.transform = 'translate(' + position.n*100 + '%, ' + position.m*100 + '%)';
         }
         if ($isSorted) doUnclickable();
         else {
@@ -34,7 +34,7 @@
     } );
 </script>
 
-<div class='tileWrapper' bind:this={tile} on:click={tileClickHandler}>
+<div class='tileWrapper' bind:this={thisElement} on:click={tileClickHandler}>
     <div class='tile'>
         {number}
     </div>
