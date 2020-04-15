040b495b84516281e5f82e6d757c975e9fb91f9d
Denis <33554650+denisnow@users.noreply.github.com>
2020-04-16 00:41:32 +0200

    deleted excessive code

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,6 +1,6 @@
 <script>
     import Tile from './Tile.svelte';
-    import { positions, respStates, tabIndexes, isSorted } from './model/stores.js';
+    import { positions, respStates, tabIndexes } from './model/stores.js';
 
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 </script>
