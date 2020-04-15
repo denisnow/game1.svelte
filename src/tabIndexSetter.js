febe2b79fa98dcb7a0a803b1960ddf2e1e9c9ee0
Denis <33554650+denisnow@users.noreply.github.com>
2020-04-15 19:49:34 +0200

    fixed formatting

--- src/tabIndexSetter.js
+++ src/tabIndexSetter.js
@@ -2,7 +2,7 @@ export default function tabIndexSetter(tileElement, tabIndex) {
 
 	function setTabIndex(tabIndex) {
 		if (tabIndex !== -1) tileElement.tabIndex = tabIndex;
-        else tileElement.removeAttribute('tabindex');
+		else tileElement.removeAttribute('tabindex');
 	}
 
 	setTabIndex(tabIndex);
