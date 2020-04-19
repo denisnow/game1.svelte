d25b0e4e42f374316e35a6be062621808d6587b0
Denis <33554650+denisnow@users.noreply.github.com>
2020-04-19 19:50:47 +0200

    commented out excessive code

--- rollup.config.js
+++ rollup.config.js
@@ -20,9 +20,11 @@ export default {
 			dev: !production,
 			// we'll extract any component CSS out into
 			// a separate file - better for performance
+			/*
 			css: css => {
 				css.write('public/build/bundle.css');
 			}
+			*/
 		}),
 
 		// If you have external dependencies installed from
